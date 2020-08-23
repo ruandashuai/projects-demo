10cdea0652cd683260069a99db217297406fbdcc
ruandashuai <295410041@qq.com>
2020-08-23 23:06:51 +0800

    上传

--- spring-boot-excel/README.md
+++ spring-boot-excel/README.md
@@ -1,6 +1,9 @@
 # 操作Excel的Demo
 #### 使用阿里巴巴的EasyExcel操作Excel
-## `状态：未完成`
+
+- [x] 上传Excel  
+- [ ] 下载Excel 
+    - 目前只能下载到项目根路径下，等待解决
 - 所需依赖：
 ```
 <dependency>
--- spring-boot-excel/src/main/java/com/example/excel/controller/StudentController.java
+++ spring-boot-excel/src/main/java/com/example/excel/controller/StudentController.java
@@ -2,21 +2,19 @@ package com.example.excel.controller;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
-import com.alibaba.excel.context.AnalysisContext;
-import com.alibaba.excel.event.AnalysisEventListener;
-import com.alibaba.excel.read.builder.ExcelReaderBuilder;
-import com.alibaba.excel.read.builder.ExcelReaderSheetBuilder;
-import com.alibaba.excel.read.metadata.ReadSheet;
 import com.example.excel.entity.Student;
 import com.example.excel.listener.ExcelListener;
 import com.example.excel.utils.JsonResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,8 +24,9 @@ import java.util.List;
 public class StudentController {
 
     @RequestMapping("/upload")
-    public JsonResult upload(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
+    public JsonResult upload(@RequestParam("file") MultipartFile file) {
         ExcelReader excelReader = null;
+        List<Student> studentList = null;
         try {
             /**
              * 1、拿到excel对象（输入流、行的实体类型class，监听器），业务操作可以放到监听器中进行
@@ -43,7 +42,7 @@ public class StudentController {
             /**
              * 返回Excel所有的行，装到一个List中
              */
-            List<Student> studentList = EasyExcel.read(file.getInputStream(), Student.class, new ExcelListener()).doReadAllSync();
+            studentList = EasyExcel.read(file.getInputStream(), Student.class, new ExcelListener()).doReadAllSync();
             for (int i = 0; i < studentList.size(); i++) {
                 System.out.println(studentList.get(i));
             }
@@ -51,8 +50,27 @@ public class StudentController {
             e.printStackTrace();
         } finally {
         }
-        return new JsonResult().setCode(1).setMessage("上传成功！").setData(null);
+        return new JsonResult().setCode(1).setMessage("上传成功！").setData(studentList);
     }
 
 
+    @RequestMapping("/download")
+    public JsonResult download(HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-disposition", "attachment;filename=学生表.xls");
+        EasyExcel.write("学生表.xls", Student.class)
+                .sheet("学生表")
+                .doWrite(getStudentList());
+        return new JsonResult().setCode(1).setMessage("下载成功！").setData(null);
+    }
+
+    private List getStudentList() {
+        ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Student student = new Student(i, "名字" + i, i, new Date());
+            students.add(student);
+        }
+        return students;
+    }
 }
